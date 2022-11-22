97b9a179d72618779abfc76ce42457099991675e
Hiszpan_ <wojtek.szacon@gmail.com>
2022-11-22 10:15:16 +0100

    Dodano layout przy todolist żeby się fragment nie powtarzał

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.todo')
 
-        <title>Lista</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-            table, td {
-                border: 1px solid black;
-                border-collapse: collapse;
-            }
-        </style>
-    </head>
-    <body>
+@section('content')
             <table>
                 <tr><td>Nazwa</td><td>Akcje</td></tr>
                 @foreach ($entries as $entry)
@@ -30,6 +11,4 @@
                 @csrf
                 <button type="submit">Dodaj</button>
             </form>
-            
-    </body>
-</html>
+@endsection()
